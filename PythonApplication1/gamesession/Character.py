0d293691657e3f6ef7598ad0aa8ac0a7222110f8
Krypticdator <toni.nurmi@hotmail.com>
2014-03-10 11:15:11 +0200

    Revert "1.02"
    This reverts commit a190fbf6c8a965ff88473630fa87aa3a8a1556ff.

--- PythonApplication1/gamesession/Character.py
+++ PythonApplication1/gamesession/Character.py
@@ -5,46 +5,18 @@ class Character(GameObject):
     def __init__(self):
         super().__init__()
 
-    def load(name):
-        pass
-
-    def save():
-        pass
-
 class Stat(GameObject):
-    def __init__(self, name='undefined', type='skill', stat='int', lvl=0, ip=0, diff=1, chip=False, frequency=0, importance=0, intensity=0):
+    def __init__(self, type='skill', stat='int', lvl=0, ip=0, diff=1, chip=False):
         super().__init__()
-        self.set_attribute('name', name)
-        self.set_attribute('type', type)
-        self.set_attribute('lvl', lvl)
         if type=='skill':
             self.set_attribute('stat', stat)
+            self.set_attribute('lvl', lvl)
             self.set_attribute('ip', ip)
             self.set_attribute('diff',diff)
             self.set_attribute('chip', chip)
-        elif type=='complication':
+        if type=='complication':
             self.set_attribute('frequency', frequency)
-            self.set_attribute('intensity', intensity)
-            self.set_attribute('importance', importance)
-
-    def increase_experience(self, exp):
-        pool = self.get_attribute('ip')
-        lvl = self.get_attribute('lvl')
-        diff_mod = self.get_attribute('diff')
-        cp_mod=1 #Kuinka nopeasti tasot kasvavat
-        pool += exp
-        
-
-        while pool>(lvl*diff_mod*cp_mod):
-            limit = (lvl +1) * diff_mod * cp_mod
-            pool -=limit
-            lvl = lvl +1
-
-        self.set_attribute('lvl',lvl)
-        self.set_attribute('ip',pool)
-
-        
-            
+            self.set_attribute
 
 
 class Effect(GameObject):
